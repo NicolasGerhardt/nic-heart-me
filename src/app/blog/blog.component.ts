74a66b45780783091b046bd1632e14ff7f30460c
Gerhardt, Nicolas (IT) <NicolasGerhardt@quickenloans.com>
2020-12-09 16:46:52 -0500

    updated blog post

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -14,8 +14,8 @@ export class BlogComponent implements OnInit {
   }
 
   post:BlogPost = {
-    title:"Hello World",
-    createdAt: new Date(2020, 2,24),
-    content: "Well I'm creating a website for my Grand Circus class, and we are hard coding html sites. I'm not yet using any sort of content management system, so this is all just written directly into the html and hosted on GitHub. I setup a GitHub pages site a while ago, so I was able to easily get this up and going.\nOne big thing that is a challenge for this lab is coming up with content for a website in a short period of time. But Content generation is what I need to do so this is content for you to consume. "
+    title:"Hello World (again)",
+    createdAt: new Date(2020, 11, 9),
+    content: "Taking some time to recreate my website and making while using a lot of the new skills that I've aquired in my journey to become a Full Stack Software Engineer.\n\nI've created this site using Anuglar and I'm hosting it on Firebase with the intention of turning this into a full blog/content delivery appication. I know there are plenty of tools that would allow for me to create this in no time, but I've never been on to back down from a challange."
   }
 }
