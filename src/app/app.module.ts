12e668364f03f002e760ce764801b285ef4af177
Gerhardt, Nicolas (IT) <NicolasGerhardt@quickenloans.com>
2020-12-11 15:37:32 -0500

    Added basic authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { BlogComponent } from './blog/blog.component';
-import { BlogPostComponent } from './blog-post/blog-post.component';
-import { AboutMeComponent } from './about-me/about-me.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogPostComponent } from './components/blog-post/blog-post.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
   { path: 'about', component: AboutMeComponent },
   { path: 'blog', component: BlogComponent },
-  { path: '', redirectTo: '/blog', pathMatch: 'full' }
+  { path: '', component: BlogComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -22,13 +27,16 @@ const routes: Routes = [
     NavbarComponent,
     BlogComponent,
     BlogPostComponent,
-    AboutMeComponent
+    AboutMeComponent,
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BlogPost } from '../interfaces/blog-post';
+import { BlogPost } from '../../interfaces/blog-post';
 
 @Component({
   selector: 'app-blog',
