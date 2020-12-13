3c6fb1a0608dda74d430e4a3281730cdce6fca58
NicolasGerhardt <Nicolas.Gerhardt@gmail.com>
2020-12-12 19:50:33 -0500

    Feature/fire auth (#5)
    * Added basic authentication
    
    * moved signin/out button to NavBar
    
    * visual update to signin button
    
    * setup login page
    
    * fixed navbar mobile toggle
    
    * updated gitignore
    
    * added firebase enviroment settings
    
    Co-authored-by: Gerhardt, Nicolas (IT) <NicolasGerhardt@quickenloans.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCbRhKAh3PNvUdHoaIodsb1JVRATM5cubA',
+    authDomain: 'nic-heart-me.firebaseapp.com',
+    projectId: 'nic-heart-me',
+    storageBucket: 'nic-heart-me.appspot.com',
+    messagingSenderId: '309000754092',
+    appId: '1:309000754092:web:6bafd5a647f3946c4c6009',
+  },
 };
