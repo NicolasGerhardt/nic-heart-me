e22eb3f4d4e91944dcb70a5c940fc997bfd6abaf
NicolasGerhardt <nicolas.gerhardt@gmail.com>
2020-12-12 19:27:19 -0500

    fixed navbar mobile toggle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+const _collapseClass = 'collapse';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -11,4 +13,13 @@ export class NavbarComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggleNavBarCollapse(): void {
+    let navbar = document.querySelector('#navbarSupportedContent');
+    if (navbar.classList.contains(_collapseClass)) {
+      navbar.classList.remove(_collapseClass);
+    } else {
+      navbar.classList.add(_collapseClass);
+    }
+  }
 }
