81d345303b45706b487b1ff0574ae2f4357da34e
Gerhardt, Nicolas (IT) <NicolasGerhardt@quickenloans.com>
2021-01-20 21:46:59 -0500

    Basic Blog Edit Screen

--- src/app/components/blog-edit/blog-edit.component.ts
+++ src/app/components/blog-edit/blog-edit.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogPost } from '../../interfaces/blog-post';
 
 @Component({
   selector: 'app-blog-edit',
   templateUrl: './blog-edit.component.html',
-  styleUrls: ['./blog-edit.component.css']
+  styleUrls: ['./blog-edit.component.css'],
 })
 export class BlogEditComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  post: BlogPost = {
+    title: 'Hello World (again)',
+    createdAt: new Date(2020, 11, 9),
+    content:
+      "Taking some time to recreate my website and making while using a lot of the new skills that I've aquired in my journey to become a Full Stack Software Engineer.\n\nI've created this site using Anuglar and I'm hosting it on Firebase with the intention of turning this into a full blog/content delivery appication. I know there are plenty of tools that would allow for me to create this in no time, but I've never been on to back down from a challange.",
+  };
 }
